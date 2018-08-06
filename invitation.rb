5d1b8c04bcd90e4c4fad8d655fbd6c520046dd65
lac417 <lac417@gmail.com>
2018-08-06 19:41:16 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -16,10 +16,10 @@ time = gets.chomp
 puts "Who is the host?"
 host = gets.chomp
 
-"Dear #{guest},
+puts "Dear #{guest},
 
 You are cordially invited to the #{party} on #{date} at #{time}. Please RSVP no later than October 30.
 
 Sincerely,
 
-#{host}
+#{host}"
